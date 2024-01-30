f9311e91c6bb7f6558f4734e04b15e188e146b6d
yutaro okuda <yutaro.okuda.76@gmail.com>
2024-01-31 07:42:58 +0800

    fix css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, StrictMode } from "react";
 import { createRoot } from 'react-dom/client';
+import "./index.css";
 
 const initialTodos: Todo[] = [
   {
